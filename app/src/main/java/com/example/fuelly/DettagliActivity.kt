e554dcff0a3316b0846708ce285e2163350044f6
LAPTOP-SEQS3LC5\mauro mandolini <st10136@iismarconipieralisi.it>
2026-04-20 11:54:46 +0200

    ottieni indicazioni
    ottieni indicazioni fatto

--- app/src/main/java/com/example/fuelly/DettagliActivity.kt
+++ app/src/main/java/com/example/fuelly/DettagliActivity.kt
@@ -1,6 +1,10 @@
 package com.example.fuelly
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
@@ -16,6 +20,8 @@ class DettagliActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_dettagli)
+
+        setSupportActionBar(findViewById(R.id.toolbar))
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -36,6 +42,29 @@ class DettagliActivity : AppCompatActivity() {
             //cerco nella lista statica delle colonnine EV l'elemento con l'id corrispondente
             val colonnina = ColonninaEV.listaVicini.find { it.id.toLong() == idRicevuto }
             colonnina?.let { setupUIElettrica(it) }
+        }
+
+        val bottone = findViewById<Button>(R.id.btnOttieniIndicazioni)
+
+        bottone.setOnClickListener {
+            //in base al tipo di elemento, chiamo la funzione di setup corretta
+            if (tipoRicevuto == "BENZINA") {
+                //cerco nella lista statica dei benzinai vicini l'elemento con l'id corrispondente
+                val stazione = Benzinaio.listaVicini.find { it.id.toLong() == idRicevuto }
+
+                val intent = Intent(Intent.ACTION_VIEW)
+                intent.data = Uri.parse("geo:0,0?q=" + stazione?.lat + "," + stazione?.lon)
+                startActivity(intent)
+
+            } else if (tipoRicevuto == "EV") {
+                //cerco nella lista statica delle colonnine EV l'elemento con l'id corrispondente
+                val colonnina = ColonninaEV.listaVicini.find { it.id.toLong() == idRicevuto }
+
+                val intent = Intent(Intent.ACTION_VIEW)
+                intent.data = Uri.parse("geo:0,0?q=" + colonnina?.lat + "," + colonnina?.lon)
+                startActivity(intent)
+            }
+
         }
     }
 
